b52e5ed2e086ca7560586ca7df2e4609f9fa086d
Joshua Avalon <joshuaavalon@gmail.com>
2018-07-28 22:41:25 +0800

    Fix package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name="plex-schema",
-      version="1.0.0",
+setup(name="plex_schema",
+      version="1.0.1",
       author="Alice Media",
       url="https://git.joshuaavalon.io/alice/plex-schema",
       python_requires=">=3.7",
