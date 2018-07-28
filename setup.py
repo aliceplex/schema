7b97152edb2c3bb5fa0f71596a028a484bd6e684
Joshua Avalon <joshuaavalon@gmail.com>
2018-07-28 21:37:22 +0800

    Fix pytest and add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="plex-schema",
+      version="1.0.0",
+      author="Alice Media",
+      url="https://git.joshuaavalon.io/alice/plex-schema",
+      python_requires=">=3.7",
+      packages=find_packages())
