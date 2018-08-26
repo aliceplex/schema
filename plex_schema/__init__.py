92c238d62be0a43cbbfcf19be5ea4c3896650db1
Joshua Avalon <joshuaavalon@gmail.com>
2018-08-26 11:22:12 +0800

    Fix test

--- plex_schema/__init__.py
+++ plex_schema/__init__.py
@@ -7,3 +7,10 @@ from .schema import ActorSchema, ActorStrictSchema, AlbumSchema, \
     AlbumStrictSchema, ArtistSchema, ArtistStrictSchema, EpisodeSchema, \
     EpisodeStrictSchema, MovieSchema, MovieStrictSchema, PersonSchema, \
     PersonStrictSchema, ShowSchema, ShowStrictSchema
+
+__all__ = ["Actor", "Album", "Artist", "Episode", "Movie", "Person", "Show",
+           "ActorSchema", "ActorStrictSchema", "AlbumSchema",
+           "AlbumStrictSchema", "ArtistSchema", "ArtistStrictSchema",
+           "EpisodeSchema", "EpisodeStrictSchema", "MovieSchema",
+           "MovieStrictSchema", "PersonSchema", "PersonStrictSchema",
+           "ShowSchema", "ShowStrictSchema"]
